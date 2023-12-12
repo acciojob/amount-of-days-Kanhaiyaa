fa5d90db93ef98601e03c4ba168e4abcdf9c8f99
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-12 11:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+function daysOfYear(year) {
+	let isleap=false
+
+	if (year%4 ==0) {
+		if (year%100 ===0) {
+			return year%400 ===0? 365:366
+		}
+		return 366
+	}
+	return 365
+}
